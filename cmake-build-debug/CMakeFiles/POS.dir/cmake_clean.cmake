27cc96adc22ba3f5d4b7027e703e12fdfc848319
Andrej Brnusak <adobrnusak@gmail.com>
2024-01-04 22:10:57 +0100

    settings

---
+++ cmake-build-debug/CMakeFiles/POS.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POS.dir/main.c.o"
+  "POS"
+  "POS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/POS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
