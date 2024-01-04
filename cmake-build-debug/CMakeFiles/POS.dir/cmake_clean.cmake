0698643a593d126d7f639bdf63899c96ce2d633f
BobceksPC <bobcekmarian@gmail.com>
2024-01-04 14:02:29 +0100

    pridany socket passive

---
+++ cmake-build-debug/CMakeFiles/POS.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/POS.dir/client.c.o"
+  "CMakeFiles/POS.dir/main.c.o"
+  "CMakeFiles/POS.dir/server.c.o"
+  "CMakeFiles/POS.dir/server/pos_sockets/active_socket.c.o"
+  "CMakeFiles/POS.dir/terminal_display.c.o"
+  "POS"
+  "POS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/POS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
