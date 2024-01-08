f74a501f1566b0ee7dcc59b892c37fe3e85bdb65
Andrej Brnusak <adobrnusak@gmail.com>
2024-01-08 16:27:35 +0100

    server, klient
    urobena komunikacia medzi serverom a dvomi klientami
    kazdy klien ma po 2 sockety (read a write)

--- client.c
+++ client.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -7,61 +6,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <time.h>
 #include <stdbool.h>
 #include <pthread.h>
 
-typedef struct klient {
-    char msg[256];
-    int sockfd;
+typedef struct client {
+    bool koniec;
     pthread_mutex_t mutex;
-} KLIENT;
+} CLIENT;
 
-bool receiveMsg (int sockfd, char* buffer) {
-    bzero(buffer,256);
-    int n;
-    n = read(sockfd, buffer, 255);
-    if (n < 0)
-    {
-        perror("Error reading from socket");
-        return false;
-    }
-    return true;
-}
+typedef struct receive {
+    int readSocket;
+    CLIENT * client;
+} RECEIVE;
 
-bool sendMsg (int sockfd, char* buffer) {
-    int n = write(sockfd, buffer, strlen(buffer));
-    if (n < 0)
-    {
-        perror("Error writing to socket");
-        return false;
-    }
-    return true;
-}
+typedef struct send {
+    int writeSocket;
+    CLIENT * client;
+} SEND;
 
-void* klientF (void *arg) {
-    KLIENT *klient = arg;
-
-    while (strcmp(klient->msg, "koniec\0") != 0) {
-        bzero(klient->msg, 256);
-        receiveMsg(klient->sockfd, klient->msg);
-        printf("%s\n", klient->msg);
-    }
-    sendMsg(klient->sockfd, "koniec\0");
-    return NULL;
-}
-
-int main(int argc, char *argv[])
-{
-    int sockfd, n;
+int config (int * readSocket, int * writeSocket) {
     struct sockaddr_in serv_addr;
     struct hostent* server;
 
     server = gethostbyname("frios2.fri.uniza.sk");
-    if (server == NULL)
-    {
+    if (server == NULL) {
         fprintf(stderr, "Error, no such host\n");
-        return 2;
+        return -2;
     }
 
     bzero((char*)&serv_addr, sizeof(serv_addr));
@@ -73,49 +43,139 @@ int main(int argc, char *argv[])
     );
     serv_addr.sin_port = htons(10002);
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
+    *readSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (*readSocket < 0) {
         perror("Error creating socket");
-        return 3;
+        return -3;
     }
-
-    if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if(connect(*readSocket, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("Error connecting to socket");
-        return 4;
+        return -4;
+    }
+    *writeSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (*writeSocket < 0) {
+        perror("Error creating socket");
+        return -3;
     }
 
-    KLIENT klientData;
-    klientData.sockfd = sockfd;
-    pthread_mutex_init(&klientData.mutex,NULL);
-
-    pthread_t klient;
-    pthread_create(&klient, NULL, klientF, &klientData);
-
-    while (strcmp(klientData.msg, "koniec\0") != 0) {
-        pthread_mutex_lock(&klientData.mutex);
-        bzero(klientData.msg,256);
-        scanf("%s", klientData.msg);
-        sendMsg(klientData.sockfd, klientData.msg);
-        pthread_mutex_unlock(&klientData.mutex);
+    if(connect(*writeSocket, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        close(*readSocket);
+        perror("Error connecting to socket");
+        return -4;
     }
-
-    pthread_join(klient,NULL);
-
-    pthread_mutex_destroy(&klientData.mutex);
-    close(sockfd);
 
     return 0;
 }
 
+int sendMsg (int writeSocket, char* buffer) {
+    int n;
+    n = write(writeSocket, buffer, strlen(buffer));
+    if (n < 0)
+    {
+        perror("Error writing to socket");
+        return -5;
+    }
 
+    return 0;
+}
 
+int receiveMsge (int readSocket, char* buffer) {
+    int n;
+    bzero(buffer,50);
+    n = read(readSocket, buffer, 49);
+    if (n < 0)
+    {
+        perror("Error reading from socket");
+        return -6;
+    }
 
-=======
->>>>>>> origin/master
-//
-// Created by PC1 on 04/01/2024.
-//
+    return 0;
+}
 
-#include "client.h"
+void * move (void * arg) {
+    SEND * send = arg;
+    char buffer[50];
+
+    while (true) {
+        pthread_mutex_lock(&send->client->mutex);
+        if (send->client->koniec) {
+            pthread_mutex_unlock(&send->client->mutex);
+            break;
+        }
+        pthread_mutex_unlock(&send->client->mutex);
+        printf("move: ");
+        scanf(" %s", buffer);
+        pthread_mutex_lock(&send->client->mutex);
+        if (strcmp(buffer, "k") == 0) {
+            sendMsg(send->writeSocket, buffer);
+            send->client->koniec = true;
+            pthread_mutex_unlock(&send->client->mutex);
+            break;
+        }
+        //pohyb v smere a az potom poslanie udajov
+        sendMsg(send->writeSocket, buffer);
+        pthread_mutex_unlock(&send->client->mutex);
+        usleep(1000);
+    }
+
+    return NULL;
+}
+
+void * update (void * arg) {
+    RECEIVE * receive = arg;
+    char buffer[50];
+    bzero(buffer, 50);
+
+    while (true) {
+        pthread_mutex_lock(&receive->client->mutex);
+        if (receive->client->koniec) {
+            pthread_mutex_unlock(&receive->client->mutex);
+            break;
+        }
+        pthread_mutex_unlock(&receive->client->mutex);
+        receiveMsge(receive->readSocket, buffer);
+        printf("%s\n", buffer);
+        //vypis na aktualneho pola
+        //pthread_mutex_unlock(&receive->client->mutex);
+        usleep(1000);
+    }
+
+    return NULL;
+}
+
+int main() {
+    int error;
+    CLIENT clientData;
+    clientData.koniec = false;
+    pthread_mutex_init(&clientData.mutex, NULL);
+
+    RECEIVE receiveData;
+    receiveData.client = &clientData;
+
+    SEND sendData;
+    sendData.client = &clientData;
+
+    error = config(&receiveData.readSocket, &sendData.writeSocket);
+    if (error) {
+        return abs(error);
+    }
+
+    sendMsg(sendData.writeSocket, "start");
+
+    pthread_t receiving;
+    pthread_t sending;
+
+    pthread_create(&sending, NULL, move, &sendData);
+    pthread_create(&receiving, NULL, update, &receiveData);
+
+    pthread_join(sending, NULL);
+    pthread_join(receiving, NULL);
+
+    printf("oka\n");
+
+    pthread_mutex_destroy(&clientData.mutex);
+    close(receiveData.readSocket);
+    close(sendData.writeSocket);
+
+    return 0;
+}
--- server.c
+++ server.c
@@ -1,4 +1,3 @@
-#include "server.h"
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -9,295 +8,213 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-typedef struct threadData {
-    bool start;
+#define MAX_CLIENTS 2
+
+typedef struct server {
+    int socket;
     bool koniec;
     pthread_mutex_t mutex;
-} THREAD_DATA;
+} SERVER;
 
-typedef struct player {
-    int serverSock;
-    int playerSock;
+typedef struct client {
     int id;
-    char data[100];
-    char msg[256];
-    THREAD_DATA threadData;
-} PLAYER;
+    int writeSocket;
+    int readSocket;
+    char data[50];
+    SERVER * server;
+} CLIENT;
 
-typedef struct game {
+typedef struct timer {
     int time;
-    int** board;
-    PLAYER* players;
-    THREAD_DATA threadData;
-} GAME;
+    CLIENT * clients[MAX_CLIENTS];
+    SERVER * server;
+} TIMER;
 
-bool receiveMsg (int sockfd, char* buffer) {
-    bzero(buffer,256);
-    int n = read(sockfd, buffer, 255);
-    if (n < 0)
-    {
-        perror("Error reading from socket");
-        return false;
-    }
-    return true;
-}
-
-bool sendMsg (int sockfd, char* buffer) {
-    int n = write(sockfd, buffer, strlen(buffer)+1);
-    if (n < 0)
-    {
-        perror("Error writing to socket");
-        return false;
-    }
-    return true;
-}
-
-void* hracF (void *arg) {
-    PLAYER *player = arg;
-
-    struct sockaddr_in cli_addr;
-    socklen_t cli_len = sizeof(cli_addr);
-    player->playerSock = accept(player->serverSock, (struct sockaddr*)&cli_addr, &cli_len); // akceptovanie klienta
-    if (player->playerSock < 0)
-    {
-        perror("ERROR on accept");
-        return NULL;
-    }
-
-    player->threadData.start = true;
-
-    pthread_mutex_lock(&player->threadData.mutex);
-    sendMsg(player->playerSock, "start\0");
-    pthread_mutex_unlock(&player->threadData.mutex);
-
-    while (strcmp(player->msg, "koniec\0") != 0) {
-        pthread_mutex_lock(&player->threadData.mutex);
-        receiveMsg(player->playerSock, player->msg);
-        printf("HRAC %d: %s\n", player->id, player->msg);
-        pthread_mutex_unlock(&player->threadData.mutex);
-    }
-    pthread_mutex_lock(&player->threadData.mutex);
-    player->threadData.koniec = true;
-    pthread_mutex_unlock(&player->threadData.mutex);
-
-    close(player->playerSock);
-    return NULL;
-}
-
-void* timeF (void *arg) {
-    GAME *game = arg;
-    while (!game->players[0].threadData.start || !game->players[1].threadData.start) {
-
-    }
-    while (!game->players[0].threadData.koniec && !game->players[1].threadData.koniec) {
-        if (game->time == 0) {
-            printf("SERVER: cas vyprsal!\n");
-            pthread_mutex_lock(&game->threadData.mutex);
-            game->players[0].threadData.koniec = true;
-            pthread_mutex_unlock(&game->threadData.mutex);
-        } else {
-            printf("SERVER: zostavajuci cas: %d\n", game->time);
-            pthread_mutex_lock(&game->threadData.mutex);
-            sendMsg(game->players[0].playerSock, "1\0");
-            sendMsg(game->players[1].playerSock, "0\0");
-            pthread_mutex_unlock(&game->threadData.mutex);
-        }
-        game->time--;
-        sleep(1);
-    }
-
-    pthread_mutex_lock(&game->threadData.mutex);
-    sendMsg(game->players[0].playerSock, "koniec\0");
-    sendMsg(game->players[1].playerSock, "koniec\0");
-    pthread_mutex_unlock(&game->threadData.mutex);
-
-    return NULL;
-}
-
-int main(int argc, char *argv[])
-{
-    printf("SERVER: spustam server!\n");
+int serverConfig () {
     int sockfd;
     struct sockaddr_in serv_addr;
-
-    bzero((char*)&serv_addr, sizeof(serv_addr)); // inicializacia sietovej adresy
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(10002);
-
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); // vytvorenie socketu
-    if (sockfd < 0)
-    {
-        perror("Error creating socket");
-        return 1;
-    }
-
-    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) // priradenie adresu socketu
-    {
-        perror("Error binding socket address");
-        return 2;
-    }
-
-    listen(sockfd, 5); // prijimanie klientov
-
-    printf("SERVER: cakam na pripojenie hracov!\n");
-
-    THREAD_DATA threadData;
-    threadData.start = false;
-    threadData.koniec = false;
-    pthread_mutex_init(&threadData.mutex,NULL);
-
-    PLAYER playerData[2];
-    for (int i = 0; i < 2; ++i) {
-        playerData[i].id = i;
-        playerData[i].serverSock = sockfd;
-        playerData[i].threadData = threadData;
-    }
-
-    GAME gameData;
-    gameData.time = 60;
-    gameData.board = (int**)calloc(5, sizeof(int*));
-    for (size_t i = 0; i < 5; ++i) {
-        gameData.board[i] = (int*)calloc(5, sizeof(int));
-    }
-    gameData.players = playerData;
-    gameData.threadData = threadData;
-
-    pthread_t players[2];
-    pthread_t timer;
-    for (int i = 0; i < 2; ++i) {
-        pthread_create(&players[i], NULL, hracF, &playerData[i]);
-    }
-    pthread_create(&timer, NULL, timeF, &gameData);
-
-    for (int i = 0; i < 2; ++i) {
-        pthread_join(players[i], NULL);
-    }
-    pthread_join(timer, NULL);
-
-    pthread_mutex_destroy(&threadData.mutex);
-    close(sockfd);
-    free(gameData.board);
-
-    return 0;
-}
-
-
-
-
-
-//
-// Created by PC1 on 04/01/2024.
-//
-
-#include "server.h"
-/*int server(int argc, char *argv[]){
-    srand(time(NULL));
-    int sockfd;
-    struct sockaddr_in serv_addr;
-
-    if (argc < 2)
-    {
-        fprintf(stderr,"usage %s port\n", argv[0]);
-        return 1;
-    }
 
     bzero((char*)&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(atoi(argv[1]));
+    serv_addr.sin_port = htons(10002);
 
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0)
-    {
+    if (sockfd < 0) {
         perror("Error creating socket");
-        return 1;
+        return -1;
     }
 
-    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
-    {
+    if (bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("Error binding socket address");
-        return 2;
-    }
-    listen(sockfd, 5);
-    printf("------------------------------------------------------\n");
-    printf("Server sa  spustil, nastavte parametre hry.\n");
-    printf("------------------------------------------------------\n\n");
-
-    while (maxPocetHracov < 2 || maxPocetHracov > 3){
-        printf("*** Vyberte z maximalneho poctu hracov (2-3) ***\n");
-        scanf("%d",&maxPocetHracov);
-        if (maxPocetHracov < 2 || maxPocetHracov > 3){
-            printf("Zadali ste nespravnu volbu, zadajte znova !\n");
-        }
+        return -2;
     }
 
-    if (maxPocetHracov == 2){
-        while (velkostMapy < 3 || velkostMapy > 9){
-            printf("*** Vyberte z rozmedzia velkosti mapy, minimalne (3), maximalne (9) ***\n");
-            scanf("%d",&velkostMapy);
-            if (velkostMapy < 3 || velkostMapy > 9){
-                printf("Zadali ste nespravnu volbu, zadajte znova !\n");
-            }
-        }
-    } else{
-        while (velkostMapy < 5 || velkostMapy > 9){
-            printf("*** Vyberte z rozmedzia velkosti mapy, minimalne (5), maximalne (9) ***\n");
-            scanf("%d",&velkostMapy);
-            if (velkostMapy < 5 || velkostMapy > 9){
-                printf("Zadali ste nespravnu volbu, zadajte znova !\n");
-            }
-        }
+    printf("SERVER: Cakam na pripojenie hracov\n");
+    listen(sockfd, 4);
+    return sockfd;
+}
+
+int clientConfig (int serverSocket, int * writeSocket, int * readSocket) {
+    socklen_t writeLen, readLen;
+    struct sockaddr_in writeAddr, readAddr;
+
+    writeLen = sizeof(writeAddr);
+    readLen = sizeof(readAddr);
+
+    *writeSocket = accept(serverSocket, (struct sockaddr*)&writeAddr, &writeLen);
+    if (*writeSocket < 0)
+    {
+        perror("ERROR on accept write socket");
+        return -3;
     }
 
-    zacinajuci = rand() % maxPocetHracov;
-    char **mapa = malloc(velkostMapy * sizeof(char *));
-    for (int i = 0; i < velkostMapy; ++i) {
-        mapa[i] = malloc(velkostMapy* sizeof(char));
-        for(int j=0; j<velkostMapy; j++){
-            mapa[i][j] = '-' ;
-        }
+    *readSocket = accept(serverSocket, (struct sockaddr*)&readAddr, &readLen);
+    if (*readSocket < 0)
+    {
+        close(*writeSocket);
+        perror("ERROR on accept read socket");
+        return -3;
     }
-
-    printf("------------------------------------------------------\n");
-    printf("Hra bola uspesne nastavena s parametrami:\npocet hracov %d\nvelkost mapy:%dx%d\n",maxPocetHracov,velkostMapy,velkostMapy);
-    printf("------------------------------------------------------\n\n");
-
-    pthread_t hraci[maxPocetHracov], kontrola;
-    pthread_mutex_t mutex;
-    pthread_mutex_init(&mutex,NULL);
-    pthread_barrier_t cakajHracov;
-    pthread_barrier_init(&cakajHracov,NULL,maxPocetHracov);
-
-    SERVERD servD = {0, maxPocetHracov, 0, menoVyhercu, 0, zacinajuci, mapa, velkostMapy, infoVyhry, &cakajHracov, &mutex};
-    HRACD * hraciD = malloc(maxPocetHracov * sizeof(HRACD));
-
-    printf("Caka sa na %d hracov...\n",servD.maximalnyPocHracov);
-
-    // Pthread 'create' / 'join'
-    for (int i = 0; i < maxPocetHracov; i++) {
-        hraciD[i].idHraca = i;
-        hraciD[i].sockfd = sockfd;
-        hraciD[i].infoHraca = infoHraca;
-        hraciD[i].infoHry = infoHry;
-        hraciD[i].okInfo = okInfo;
-        hraciD[i].spoluD = &servD;
-        pthread_create(&hraci[i], NULL, hraciFun, &hraciD[i]);
-    }
-
-
-    for (int i = 0; i < maxPocetHracov; i++) {
-        pthread_join(hraci[i], NULL);
-    }
-
-
-
-
-
-    printf("------------------------------------------------------\n");
-    printf("Server Skoncil\n");
-    printf("------------------------------------------------------\n\n");
-    close(sockfd);
+    printf("Pripojeny %d,%d\n", *writeSocket, *readSocket);
 
     return 0;
-}*/
+}
+
+int sendMsg (int writeSocket, char* buffer) {
+    int n;
+    n = write(writeSocket, buffer, strlen(buffer) + 1);
+    if (n < 0)
+    {
+        perror("Error writing to socket");
+        return -5;
+    }
+
+    return 0;
+}
+
+int receiveMsg (int readSocket, char* buffer) {
+    int n;
+    bzero(buffer,50);
+    n = read(readSocket, buffer, 49);
+    if (n < 0)
+    {
+        perror("Error reading from socket");
+        return -4;
+    }
+
+    return 0;
+}
+
+void* clientF (void *arg) {
+    char buffer[50];
+    CLIENT * client = arg;
+
+    while (true) {
+        pthread_mutex_lock(&client->server->mutex);
+        if (client->server->koniec) {
+            pthread_mutex_unlock(&client->server->mutex);
+            break;
+        }
+        pthread_mutex_unlock(&client->server->mutex);
+        receiveMsg(client->readSocket, buffer);
+        printf("%s\n", buffer);
+        pthread_mutex_lock(&client->server->mutex);
+        if (strcmp(buffer, "k") == 0) {
+            client->server->koniec = true;
+            pthread_mutex_unlock(&client->server->mutex);
+            break;
+        } else {
+            bzero(client->data, 50);
+            stpcpy(client->data, buffer);
+        }
+        pthread_mutex_unlock(&client->server->mutex);
+    }
+
+    return NULL;
+}
+
+void* timerF (void *arg) {
+    TIMER * timer = arg;
+
+    while (true) {
+        if (timer->time == 0) {
+            pthread_mutex_lock(&timer->server->mutex);
+            timer->server->koniec = true;
+            sendMsg(timer->clients[0]->writeSocket, "k");
+            sendMsg(timer->clients[1]->writeSocket, "k");
+            pthread_mutex_unlock(&timer->server->mutex);
+
+            break;
+        }
+        pthread_mutex_lock(&timer->server->mutex);
+        if (timer->server->koniec) {
+            pthread_mutex_unlock(&timer->server->mutex);
+            break;
+        }
+        //pthread_mutex_unlock(&timer->server->mutex);
+        /*sendMsg(timer->clients[0]->writeSocket, timer->clients[1]->data);
+        sendMsg(timer->clients[1]->writeSocket, timer->clients[0]->data);*/
+        sendMsg(timer->clients[0]->writeSocket, "prvy");
+        sendMsg(timer->clients[1]->writeSocket, "druhy");
+        pthread_mutex_unlock(&timer->server->mutex);
+
+        printf("%d\n", timer->time);
+        sleep(3);
+        timer->time -= 3;
+    }
+
+    return NULL;
+}
+
+int main() {
+    int error;
+    SERVER serverData;
+    serverData.socket = serverConfig();
+    if (serverData.socket < 0) {
+        return abs(serverData.socket);
+    }
+    serverData.koniec = false;
+    pthread_mutex_init(&serverData.mutex, NULL);
+
+    CLIENT clientData[MAX_CLIENTS];
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        clientData[i].id = i;
+        bzero(clientData[i].data, 50);
+        clientData[i].server = &serverData;
+        error = abs(clientConfig(serverData.socket, &clientData[i].writeSocket, &clientData[i].readSocket));
+        if (error) {
+            close(serverData.socket);
+            return error;
+        }
+    }
+
+    TIMER timerData;
+    timerData.time = 30;
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        timerData.clients[i] = &clientData[i];
+    }
+    timerData.server = &serverData;
+
+    pthread_t clients[MAX_CLIENTS];
+    pthread_t timer;
+
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        pthread_create(&clients[i], NULL, clientF, &clientData[i]);
+    }
+    pthread_create(&timer, NULL, timerF, &timerData);
+
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        pthread_join(clients[i], NULL);
+    }
+    pthread_join(timer, NULL);
+
+    pthread_mutex_destroy(&serverData.mutex);
+    for (int i = 0; i < MAX_CLIENTS; ++i) {
+        close(clientData[i].writeSocket);
+        close(clientData[i].readSocket);
+    }
+    close(serverData.socket);
+
+    return 0;
+}
